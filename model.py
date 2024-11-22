878038e4a8367fa49cb5b34de7f5232abef1bdf8
Murad Aghayev <muradaghyv@gmail.com>
2024-11-22 18:12:45 +0400

    adding extra functions to model definition file

--- model.py
+++ model.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from sklearn.metrics import f1_score
+
 import torch
 import torch.nn as nn
 torch.cuda.empty_cache()
@@ -84,3 +88,72 @@ class AutoEncoder(nn.Module):
         avg_loss = total_loss/num_batches   
         
         return avg_loss
+    
+    def optimal_threshold(self, validation_data, labels):
+        self.eval()
+        with torch.no_grad():
+            _, errors = self.get_reconstruction_error(validation_data.to(device))
+            
+            thresholds = np.percentile(errors.cpu().detach().numpy(), np.arange(0, 100, 5))
+            error_arr = errors.cpu().detach().numpy()
+            best_f1 = 0
+            best_threshold = None
+
+            for threshold in thresholds:
+                predictions = (error_arr > threshold)
+                f1 = f1_score(predictions, labels)
+
+                if f1 > best_f1:
+                    best_f1 = f1
+                    best_threshold = threshold
+
+        return best_threshold
+    
+    def train_model(self, train_loader, val_loader, criterion, optimizer,
+              num_epochs, device, early_stopping_patience=10):
+        
+        training_losses = []
+        validation_losses = []
+        best_val_loss = float("inf")
+        patience_encounter = 0
+
+        for epoch in range(num_epochs):
+            self.train()
+            epoch_loss = 0.0
+            batch_count = 0
+
+            for batch in train_loader:
+                batch = batch[0].to(device)
+
+                optimizer.zero_grad()
+                reconstructed = self(batch)
+                loss = criterion(reconstructed, batch)
+
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.item()
+                batch_count += 1
+            
+            avg_train_loss = epoch_loss/batch_count
+            training_losses.append(avg_train_loss)
+
+            val_loss = self.validate(val_loader, criterion, device)
+            validation_losses.append(val_loss)
+
+            if (epoch+1) % 5 == 0:
+                print(f"Epoch: {epoch}")
+                print(f"Training loss: {avg_train_loss}")
+                print(f"Validation loss: {val_loss}")
+
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                patience_encounter = 0
+                torch.save(self, "best_model.pth")
+            else:
+                patience_encounter += 1
+                if patience_encounter >= early_stopping_patience:
+                    print(f"Training stopped after {epoch} epochs!")
+                    break
+            
+        return training_losses, validation_losses
