4750eb49ef85efa8b9fcb0d493e3a917f2e8e898
Murad Aghayev <muradaghyv@gmail.com>
2024-11-22 17:45:50 +0400

    modifying testing file

--- model_testing.py
+++ model_testing.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import joblib
 
-from sklearn.metrics import accuracy_score, recall_score
+from sklearn.metrics import accuracy_score, recall_score, f1_score
 
 torch.cuda.empty_cache()
 
@@ -13,7 +13,7 @@ torch.cuda.empty_cache()
 device = ("cuda" if torch.cuda.is_available() else "cpu")
 
 # Uploading the pretrained model and setting eval mode
-model = torch.load("anomaly_detection_updated.pth")
+model = torch.load("best_model.pth")
 model = model.to(device)
 model.eval()
 
@@ -43,7 +43,7 @@ criterion = nn.MSELoss()
 predictions = []
 for i in range(df.shape[0]):
     error = criterion(outputs[i], input_tensor[i])
-    if error >= 0.1:
+    if error >= 0.03:
         predictions.append(1)
     else:
         predictions.append(0)
@@ -64,5 +64,29 @@ print(f"The Recall score for normal cases: {round(recall_0*100, 2)} %.")
 print(f"The Recall score for anomalous cases: {round(recall_1*100, 2)} %.")
 
 # Saving final results_df
-results_df.to_csv("results.csv")
+# results_df.to_csv("results.csv")
+
+def optimal_threshold(model, validation_data, labels):
+    model.eval()
+    with torch.no_grad():
+        _, errors = model.get_reconstruction_error(validation_data.to(device))
+        
+        thresholds = np.percentile(errors.cpu().detach().numpy(), np.arange(0, 100, 5))
+        error_arr = errors.cpu().detach().numpy()
+        best_f1 = 0
+        best_threshold = None
+
+        for threshold in thresholds:
+            predictions = (error_arr > threshold)
+            f1 = f1_score(predictions, labels)
+
+            if f1 > best_f1:
+                best_f1 = f1
+                best_threshold = threshold
+
+    return best_threshold
+optimal_threshold_value = optimal_threshold(model=model, validation_data=input_tensor, labels=results_df["label"])
+print(f"optimal threshold is: {optimal_threshold_value}")
+
+
 
